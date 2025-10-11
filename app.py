12676f31933e6dc5399c14e04cd027e3eac43309
alishah18105 <alishah18105@gmail.com>
2025-10-11 08:48:40 +0500

    Report Screen Added With Meeting & Participant Report

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from datetime import datetime, timedelta
+from email.mime import text
 import os
 from flask import Flask, flash, request, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-from models import Meeting, Organizer, Participant, db, User, Notification
+from models import Meeting, Organizer, Participant, Room, db, User, Notification
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from flask import redirect, session, url_for, flash
 from routes.Calendar import calendar_bp 
+from sqlalchemy import func
+
 
 
 
@@ -324,6 +327,93 @@ def view_all_notifications():
 @app.route('/logout', methods = ['GET', 'POST'])
 def logout():
     return render_template('login.html')
+
+from datetime import datetime
+from sqlalchemy import func
+
+@app.route('/report', methods=['GET', 'POST'])
+@login_required
+def report():
+    report_data = []
+    user_id = session['user_id']
+    organizer = Organizer.query.filter_by(user_id=user_id).first()
+
+    meetings = []
+    if organizer:
+        meetings = Meeting.query.filter_by(organizer_id=organizer.organizer_id).all()
+
+    if request.method == 'POST':
+        report_type = request.form.get('report_type')
+        start_date = request.form.get('start_date')
+        end_date = request.form.get('end_date')
+        selected_statuses = request.form.getlist('status')
+        title = request.form.get('title')
+
+        if report_type == 'Participant_Report':
+            if title:  # only fetch if meeting selected
+                query = (
+                    db.session.query(
+                    User.name,
+                    User.email,
+                    Participant.attendance_status,
+                    func.to_char(Participant.joined_at, 'YYYY-MM-DD HH12:MI AM').label('joined_at')
+                    )
+                    .join(Participant, Participant.user_id == User.user_id)
+                    .filter(Participant.meeting_id == title)
+                   
+                )
+                report_data = query.all()
+
+        if report_type == 'Meeting_Summary':
+            user_id = session['user_id']
+            organizer = Organizer.query.filter_by(user_id=user_id).first()
+
+            if organizer:
+                query = (
+                    db.session.query(
+                        Meeting.meeting_id,
+                        Meeting.title,
+                        func.date(Meeting.start_time).label('meeting_date'),
+                        func.to_char(Meeting.start_time, 'HH12:MI AM').label('meeting_time'),
+                        Room.room_name,
+                        Meeting.status,
+                        func.count(Participant.participant_id).label('participants')
+                    )
+                    .outerjoin(Participant, Participant.meeting_id == Meeting.meeting_id)
+                    .join(Room, Room.room_id == Meeting.room_id)
+                    .filter(Meeting.organizer_id == organizer.organizer_id)
+                    .group_by(Meeting.meeting_id, Room.room_name)
+                )
+
+                if start_date and end_date:
+                    start_dt = datetime.strptime(start_date, '%Y-%m-%d')
+                    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+                    end_dt = end_dt.replace(hour=23, minute=59, second=59)
+                    query = query.filter(Meeting.start_time.between(start_dt, end_dt))
+
+                elif start_date:  # only start date given
+                    start_dt = datetime.strptime(start_date, '%Y-%m-%d')
+                    query = query.filter(Meeting.start_time >= start_dt)
+
+                elif end_date:  # only end date given
+                    end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+                    end_dt = end_dt.replace(hour=23, minute=59, second=59)
+                    query = query.filter(Meeting.start_time <= end_dt)
+
+                if selected_statuses:
+                    query = query.filter(Meeting.status.in_(selected_statuses))
+
+               
+
+                report_data = query.all()
+
+
+    return render_template('report.html', report_data=report_data, meetings=meetings, report_type=request.form.get('report_type'))
+
+
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8000)
